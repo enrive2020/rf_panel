6fc9f48badef26176f00ff24a95207e1258cd4e7
EnRive <gg.enrive@yandex.ru>
2026-05-28 20:06:00 +0500

    Change

--- app/modules/client_manager/config.py
+++ app/modules/client_manager/config.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 
-
 APP_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = APP_DIR / "data"
 
 DB_PATH = DATA_DIR / "rf_panel.db"
 
 CLIENT_PATH = r"D:\RF-Platform\Client"
-
 
 def ensure_data_dirs():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
